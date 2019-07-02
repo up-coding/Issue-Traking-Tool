edba641051417b459c9105fbda3f1190ad1193b4
Rahul Sharma <rahulsharma15j@gmail.com>
2019-07-02 21:41:34 +0530

    updated

--- src/app/dashboard/personalized-dashboard/personalized-dashboard.component.ts
+++ src/app/dashboard/personalized-dashboard/personalized-dashboard.component.ts
@@ -1,137 +1,141 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { Cookie } from 'ng2-cookies';
-import { AppService } from 'src/app/app.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { IssueService } from 'src/app/issue.service';
-import { SocketService } from 'src/app/socket.service';
- 
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Cookie } from "ng2-cookies";
+import { AppService } from "src/app/app.service";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { IssueService } from "src/app/issue.service";
+import { SocketService } from "src/app/socket.service";
+
 declare var $: any;
 
 @Component({
-  selector: 'app-personalized-dashboard',
-  templateUrl: './personalized-dashboard.component.html',
-  styleUrls: ['./personalized-dashboard.component.css'],
-  providers:[ ]
+  selector: "app-personalized-dashboard",
+  templateUrl: "./personalized-dashboard.component.html",
+  styleUrls: ["./personalized-dashboard.component.css"],
+  providers: []
 })
 export class PersonalizedDashboardComponent implements OnInit {
   /**
-   * Allow component to add datatable to 
+   * Allow component to add datatable to
    * the view
    */
-  @ViewChild('dataTable') table: { nativeElement: any; };
-  dataTable:any;
-  dtOptions:any; 
-  
+  @ViewChild("dataTable") table: { nativeElement: any };
+  dataTable: any;
+  dtOptions: any;
+
   //authToken of a user
-  public authToken:any;
+  public authToken: any;
 
   //contains user information
-  public userInfo:any;
+  public userInfo: any;
 
   //contains user id
-  public receiverId:any;
+  public receiverId: any;
 
   //contains user name
-  public receiverName:any;
+  public receiverName: any;
 
   //contains All assignedissue to a perticular user
   public allAssignedIssue: any;
-  public list=[];
-  public disconnectedSocket:boolean;
-  public q:string;
+  public list = [];
+  public disconnectedSocket: boolean;
+  public q: string;
   public res: any;
-  public allIssueDetails:any;
-  public assigneeId:any;
+  public allIssueDetails: any;
+  public assigneeId: any;
   public newList: any[];
-  
+
   /**
    * constructor
-   * @param appSevice 
-   * @param socketService 
-   * @param issueService 
-   * @param router 
-   * @param toastr 
+   * @param appSevice
+   * @param socketService
+   * @param issueService
+   * @param router
+   * @param toastr
    */
-  constructor(public appSevice:AppService,
-    public socketService:SocketService,
-    public issueService:IssueService,
-    public router:Router,
-    private toastr:ToastrService) { 
-      
-      this.receiverId = Cookie.get('receiverId');
-      this.receiverName = Cookie.get('receiverName');
-    }
+  constructor(
+    public appSevice: AppService,
+    public socketService: SocketService,
+    public issueService: IssueService,
+    public router: Router,
+    private toastr: ToastrService
+  ) {
+    this.receiverId = Cookie.get("receiverId");
+    this.receiverName = Cookie.get("receiverName");
+  }
 
-    /**
-     * method to navigate search
-     * query to search view
-     */
-    public submitSearch(){
-        let query = this.q;
-        if(query){
-          this.router.navigate(['/search',{q:query}]);
-        }
+  /**
+   * method to navigate search
+   * query to search view
+   */
+  public submitSearch() {
+    let query = this.q;
+    if (query) {
+      this.router.navigate(["/search", { q: query }]);
     }
+  }
 
-    /**
-     * Initializing datatable to 
-     * the view
-     */
-   ngOnInit() {
+  /**
+   * Initializing datatable to
+   * the view
+   */
+  ngOnInit() {
     this.dtOptions = {
-        "paging":   true,
-        "ordering": true,
-        "info":     false,
-        "searching": false,
-        "bPaginate": false,
-        "bLengthChange": false,
-        
+      paging: true,
+      ordering: true,
+      info: false,
+      searching: false,
+      bPaginate: false,
+      bLengthChange: false
     };
     this.dataTable = $(this.table.nativeElement);
     this.dataTable.dataTable(this.dtOptions);
-    this.authToken = Cookie.get('authToken');
+    this.authToken = Cookie.get("authToken");
     this.userInfo = this.appSevice.getUserInfoFromLocalStorage();
     this.verifyUserConfirmation();
     this.getAllIssues();
   }
 
   /**
-   * Get all issue for the 
+   * Get all issue for the
    * logged in user
    */
-  public getAllIssues = ()=>{
-      this.issueService.getAllIssues().subscribe(response=>{
-        this.allIssueDetails = response.data;
-          for(let x in this.allIssueDetails){
-          this.assigneeId = this.allIssueDetails[x].assigneeId;
-          if(this.receiverId === this.assigneeId){
-             if(this.allIssueDetails[x].length < 0){
-                 continue;
-             }else{
-               this.list[x]=this.allIssueDetails[x];
-             }
+  public getAllIssues = () => {
+    this.issueService.getAllIssues().subscribe(
+      response => {
+        if (response.data) {
+          this.allIssueDetails = response.data;
+
+          for (let x in this.allIssueDetails) {
+            this.assigneeId = this.allIssueDetails[x].assigneeId;
+            console.log(this.allIssueDetails[x]);
+            if (this.receiverId === this.assigneeId) {
+              this.list[x] = this.allIssueDetails[x];
+            }
           }
-       } 
-       this.newList = this.list.filter(value => Object.keys(value).length !== 0)
-      },err=> {this.toastr.error('Some error occured')});
-    }
- 
-   /**
-    * Verify user
-    */
-   public verifyUserConfirmation:any = ()=>{
-       this.socketService.verifyUser().subscribe((data)=>{
-          this.disconnectedSocket = false;
-          this.socketService.setUser(this.authToken);
-           
-       });
-   } 
+          this.newList = this.list.filter(value => {
+            Object.keys(value).length !== 0;
+          });
+        }
+      },
+      err => {
+        this.toastr.error("Some error occured");
+      }
+    );
+  };
 
-   
+  /**
+   * Verify user
+   */
+  public verifyUserConfirmation: any = () => {
+    this.socketService.verifyUser().subscribe(data => {
+      this.disconnectedSocket = false;
+      this.socketService.setUser(this.authToken);
+    });
+  };
+
   /**Redirect to create new issue */
-  public createNewIssue = ()=>{
-    this.router.navigate(['/issue']);
-  }
-
+  public createNewIssue = () => {
+    this.router.navigate(["/issue"]);
+  };
 }
--- src/app/issue-description/issue-create/issue-create.component.ts
+++ src/app/issue-description/issue-create/issue-create.component.ts
@@ -1,186 +1,206 @@
-import { Component, OnInit,ViewChild, EventEmitter, OnDestroy } from '@angular/core';
-import { CKEditorComponent } from 'ng2-ckeditor';
-import { Location } from '@angular/common'; 
-import { ToastrService } from 'ngx-toastr';
-import { Cookie } from 'ng2-cookies';
-import { IssueService } from 'src/app/issue.service';
-import { AppService } from 'src/app/app.service';
-import { Router, ActivatedRoute } from '@angular/router';
- 
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  EventEmitter,
+  OnDestroy
+} from "@angular/core";
+import { CKEditorComponent } from "ng2-ckeditor";
+import { Location } from "@angular/common";
+import { ToastrService } from "ngx-toastr";
+import { Cookie } from "ng2-cookies";
+import { IssueService } from "src/app/issue.service";
+import { AppService } from "src/app/app.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-issue-description',
-  templateUrl: './issue-create.component.html',
-  styleUrls: ['./issue-create.component.css'],
-  providers:[Location]
+  selector: "app-issue-description",
+  templateUrl: "./issue-create.component.html",
+  styleUrls: ["./issue-create.component.css"],
+  providers: [Location]
 })
-
-export class IssueCreateComponent implements OnInit,OnDestroy {
-  
+export class IssueCreateComponent implements OnInit, OnDestroy {
   /**
-   * Adding rich text editor to 
+   * Adding rich text editor to
    * the view
    */
   @ViewChild(CKEditorComponent) ckEditor: CKEditorComponent;
- 
-  public title:string;
-  public description:any;
-  public files:[] ;
-  public reporterId:string;
-  public reporterName:string;
-  public assigneeName:[];
-  public assigneeId:string;
-  public allAssignee = [];
-  public authToken:any;
-  public status = ['In-backlog','In-Progress','In-Test','Done'];
-  public userStatus:any;
+
+  public title: string;
+  public description: any;
+  public files: [];
+  public reporterId: string;
+  public reporterName: string;
+  public assigneeName: [];
+  public assigneeId: string;
+  public allAssignee: any = [];
+  public authToken: any;
+  public status = ["In-backlog", "In-Progress", "In-Test", "Done"];
+  public userStatus: any;
   public currentIssue;
   public currentStatus;
   public assignee;
-  private myFiles:string [] = [];
-  
+  private myFiles: string[] = [];
+
   /**
    * Constructor
-   * @param issueService 
-   * @param appService 
-   * @param toastr 
-   * @param _route 
-   * @param router 
-   * @param location 
+   * @param issueService
+   * @param appService
+   * @param toastr
+   * @param _route
+   * @param router
+   * @param location
    */
-  constructor(public issueService:IssueService,
-    public appService:AppService,
-    private toastr:ToastrService,
-    private _route:ActivatedRoute,
-    public router:Router,
-    private location:Location 
-    ) {}
-  
+  constructor(
+    public issueService: IssueService,
+    public appService: AppService,
+    private toastr: ToastrService,
+    private _route: ActivatedRoute,
+    public router: Router,
+    private location: Location
+  ) {}
+
   /**
    * Initializing userdetails
    */
   ngOnInit() {
-    this.authToken = Cookie.get('authToken');
-    this.reporterId = Cookie.get('receiverId');
-    this.reporterName = Cookie.get('receiverName');
+    this.authToken = Cookie.get("authToken");
+    this.reporterId = Cookie.get("receiverId");
+    this.reporterName = Cookie.get("receiverName");
     this.getAllUsersName(this.authToken);
   }
 
-  
   //text editor configuration
-  ngAfterViewChecked(){
+  ngAfterViewChecked() {
     let editor = this.ckEditor.instance;
-    editor.config.height = '200';
-     
+    editor.config.height = "200";
+
     editor.config.toolbarGroups = [
-      {name:'document',groups:['mode','document','doctools']},
-      {name:'clipboard',groups:['clipboard','undo']},
-      {name:'editing',groups:['find','selection','spellchecker','editing']},
-      {name:'paragraph',groups:['list','indent','blocks','align','bidi','paragraph']},
-      { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-      { name: 'links', groups: [ 'links' ] },
-      { name: 'styles', groups: [ 'styles' ] },
-      { name: 'colors', groups: [ 'colors' ] }
-    ]; 
+      { name: "document", groups: ["mode", "document", "doctools"] },
+      { name: "clipboard", groups: ["clipboard", "undo"] },
+      {
+        name: "editing",
+        groups: ["find", "selection", "spellchecker", "editing"]
+      },
+      {
+        name: "paragraph",
+        groups: ["list", "indent", "blocks", "align", "bidi", "paragraph"]
+      },
+      { name: "basicstyles", groups: ["basicstyles", "cleanup"] },
+      { name: "links", groups: ["links"] },
+      { name: "styles", groups: ["styles"] },
+      { name: "colors", groups: ["colors"] }
+    ];
     editor.config.removeButtons = `Anchor,Save,Find,Replace,Scayt,SelectAll,Form,Radio`;
   }
 
   /**
-   * Method to select 
+   * Method to select
    * status from the view
-   * @param issueStatus 
+   * @param issueStatus
    */
-  public selectStatus(issueStatus){
-    this.userStatus = issueStatus
+  public selectStatus(issueStatus) {
+    this.userStatus = issueStatus;
     return this.userStatus;
- }
+  }
 
   /**
-   * method to select 
+   * method to select
    * user from the view
-   * @param id 
-   * @param name 
+   * @param id
+   * @param name
    */
-  public selectUser(id,name){
-      this.assigneeId = id;
-     this.assigneeName = name;
-     return this.assigneeId && this.assigneeName;
+  public selectUser(id) {
+    // this.assigneeId = id;
+    // this.assigneeName = name;
+    console.log(id);
+    //console.log(name);
+
+    //return this.assigneeId && this.assigneeName;
   }
-   
-   
 
   /**
    * method to get all username
    */
-  public getAllUsersName:any = (authToken)=>{
-     this.appService.getAllUsers(authToken).subscribe((apiResponse:any)=>{
-          for (let x in apiResponse.data) {
-          let temp = { 'userId': apiResponse.data[x].userId, 'name': apiResponse.data[x].firstName + ' ' + apiResponse.data[x].lastName  };
-              this.allAssignee.push(temp);          
-          }
-        });
-  }
+  public getAllUsersName: any = authToken => {
+    this.appService.getAllUsers(authToken).subscribe((apiResponse: any) => {
+      for (let x in apiResponse.data) {
+        let temp = {
+          userId: apiResponse.data[x].userId,
+          name:
+            apiResponse.data[x].firstName + " " + apiResponse.data[x].lastName
+        };
+        this.allAssignee.push(temp);
+      }
+    });
+
+    console.log(this.allAssignee);
+  };
 
   /**
    * select event to select the files
    * from event
-   * @param event 
+   * @param event
    */
   public onSelected(event) {
-    if(event.currentTarget.files.length > 0){
-        const selectedFiles = event.currentTarget.files;
-        this.files = selectedFiles;
-        for (var i = 0; i < event.target.files.length; i++) { 
-          this.myFiles.push(event.target.files[i]);
-        }
+    if (event.currentTarget.files.length > 0) {
+      const selectedFiles = event.currentTarget.files;
+      this.files = selectedFiles;
+      for (var i = 0; i < event.target.files.length; i++) {
+        this.myFiles.push(event.target.files[i]);
+      }
     }
   }
-  
+
   /**
    * onSubmit this method will
    * create new Issue
    */
-  public onSubmit = ()=>{
-    if(!this.title){
-      this.toastr.warning('Title required!');
-    }else if(!this.description){
-      this.toastr.warning('Description required!');
-    }else if(!this.assigneeName){
-      this.toastr.warning('Select assignee!');
-    }else if(!this.status){
-      this.toastr.warning('Select status!');
+  public onSubmit = () => {
+    if (!this.title) {
+      this.toastr.warning("Title required!");
+    } else if (!this.description) {
+      this.toastr.warning("Description required!");
+    } else if (!this.assigneeName) {
+      this.toastr.warning("Select assignee!");
+    } else if (!this.status) {
+      this.toastr.warning("Select status!");
     }
     const formData = new FormData();
-    for (var i = 0; i < this.myFiles.length; i++) { 
+    for (var i = 0; i < this.myFiles.length; i++) {
       formData.append("files", this.myFiles[i]);
     }
-    formData.append('title',this.title);
-    formData.append('description',this.description);
-    formData.append('status',this.userStatus);
-    formData.append('reporterId',this.reporterId);
-    formData.append('assigneeId',this.assigneeId);
-    formData.append('reporterName',this.reporterName);
-    formData.append('assigneeName',JSON.stringify(this.assigneeName));
-    this.issueService.createAIssue(formData).subscribe((apiResponse)=>{
-    if(apiResponse.status === 200){
-         this.toastr.success('Issue created successfully!','Success!');
-         setTimeout(()=>{  
-            this.router.navigate(['/issue-view',apiResponse.data.issueId]);
-        }, 1000)
-    }else{this.toastr.error(apiResponse.message,'Error');}
-    },(err)=>{
-       this.toastr.error('Something wrong happened!','Error');
-    }); 
-} 
+    formData.append("title", this.title);
+    formData.append("description", this.description);
+    formData.append("status", this.userStatus);
+    formData.append("reporterId", this.reporterId);
+    formData.append("assigneeId", this.assigneeId);
+    formData.append("reporterName", this.reporterName);
+    formData.append("assigneeName", JSON.stringify(this.assigneeName));
+    console.log(formData);
+    this.issueService.createAIssue(formData).subscribe(
+      apiResponse => {
+        if (apiResponse.status === 200) {
+          this.toastr.success("Issue created successfully!", "Success!");
+          setTimeout(() => {
+            this.router.navigate(["/issue-view", apiResponse.data.issueId]);
+          }, 1000);
+        } else {
+          this.toastr.error(apiResponse.message, "Error");
+        }
+      },
+      err => {
+        this.toastr.error("Something wrong happened!", "Error");
+      }
+    );
+  };
 
-//method to return previous page
- public goBack = ()=>{
-   this.location.back();
- }
+  //method to return previous page
+  public goBack = () => {
+    this.location.back();
+  };
 
- ngOnDestroy(): void {
-  if (this.ckEditor.instance.editor) this.ckEditor.instance.editor.destroy();
-   
+  ngOnDestroy(): void {
+    if (this.ckEditor.instance.editor) this.ckEditor.instance.editor.destroy();
+  }
 }
-}
-
