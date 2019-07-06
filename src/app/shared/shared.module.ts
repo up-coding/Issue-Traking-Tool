2191e3c284104100b692fedc76385c5050349193
Rahul Sharma <rahulsharma15j@gmail.com>
2019-07-06 20:25:25 +0530

    mobile css

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { LogoutComponent } from '../user/logout/logout.component';
-import { AuthGuard } from '../auth.guard';
- 
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { UserDetailsComponent } from "./user-details/user-details.component";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { AuthGuard } from "../auth.guard";
 
 @NgModule({
   declarations: [UserDetailsComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([
-      {path:'logout',component:LogoutComponent,canActivate:[AuthGuard]}])
-  ],
-  exports:[UserDetailsComponent
-  ,CommonModule,
-  
-  ]
-  ,providers:[AuthGuard]
+  imports: [CommonModule, FormsModule, RouterModule.forChild([])],
+  exports: [UserDetailsComponent, CommonModule],
+  providers: [AuthGuard]
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,33 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { RouterModule } from '@angular/router';
-import { LogoutComponent } from './logout/logout.component';
-import { AuthGuard } from '../auth.guard';
- 
- 
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { ToastrModule } from "ngx-toastr";
+import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from "./login/login.component";
+import { RouterModule } from "@angular/router";
 
+import { AuthGuard } from "../auth.guard";
 
 @NgModule({
-  declarations: [
-    SignupComponent,
-    LoginComponent,
-    LogoutComponent
-  ],
+  declarations: [SignupComponent, LoginComponent],
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     FormsModule,
     ToastrModule.forRoot(),
-    RouterModule.forChild([
-      {path:'signup',component:SignupComponent},
-      {path:'logout',component:LogoutComponent,canActivate:[AuthGuard]}
-    ])
+    RouterModule.forChild([{ path: "signup", component: SignupComponent }])
   ],
-  providers:[AuthGuard]
+  providers: [AuthGuard]
 })
-export class UserModule { }
+export class UserModule {}
